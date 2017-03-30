5ba55ff7d653507f8bdd84ed36769f701836ac4a
Dangaia Sims <dangaia.sims@gmail.com>
2017-03-29 21:00:27 -0700

    Update q7_lists.py

--- python/q7_lists.py
+++ python/q7_lists.py
@@ -3,19 +3,11 @@
 
 
 def match_ends(words):
-    """
-    Given a list of strings, return the count of the number of strings
-    where the string length is 2 or more and the first and last chars
-    of the string are the same.
-
-    >>> match_ends(['aba', 'xyz', 'aa', 'x', 'bbb'])
-    3
-    >>> match_ends(['', 'x', 'xy', 'xyx', 'xx'])
-    2
-    >>> match_ends(['aaa', 'be', 'abc', 'hello'])
-    1
-    """
-    raise NotImplementedError
+    count = 0
+    for word in words:
+        if len(word) >= 2 and word[0] == word[-1]:
+            count = count + 1
+    return count
 
 
 def front_x(words):
