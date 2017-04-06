af2ac02db70bc1843d7667e16cceb21a0067e1e0
Dangaia Sims <dangaia.sims@gmail.com>
2017-04-06 15:34:16 -0400

    Completed

--- python/advanced_python_dict.py
+++ python/advanced_python_dict.py
@@ -5,8 +5,10 @@ import re
 
 filename = 'faculty.csv'
 
-def make_faculty_dict():
-	faculty_dict = {}
+faculty_dict = {}
+
+def new_faculty_dict():
+	
 	with open(filename, 'r') as f:
 		faculty_csv = csv.reader(f, delimiter=',')
 		next(faculty_csv)
@@ -27,7 +29,9 @@ def make_faculty_dict():
 
 	return faculty_dict
 
-print (make_faculty_dict())
+for key, value in sorted(new_faculty_dict().items())[:3]:
+	print (key, value)
+#I tried using itertools/islice here but I couldn't get it to work properly.
 
 #Q7: 
 
@@ -36,7 +40,7 @@ import re
 
 filename = 'faculty.csv'
     
-def make_professor_dict():
+def new_professor_dict():
 	professor_dict = {}
 	with open(filename, 'r') as f:
 		faculty_csv = csv.reader(f, delimiter=',')
@@ -58,9 +62,37 @@ def make_professor_dict():
 
 	return professor_dict
 
-for k, v in sorted(make_professor_dict().items())[:3]:
-	print (k, v)
+for key, value in sorted(new_professor_dict().items())[:3]:
+	print (key, value)
     
 #Q8:
-for k, v in sorted(make_prof().items(), key = lambda k: k[0][1])[:3]:
-	print (k, v)
+
+import csv
+import re
+
+filename = 'faculty.csv'
+    
+def new_professor_dict():
+	professor_dict = {}
+	with open(filename, 'r') as f:
+		faculty_csv = csv.reader(f, delimiter=',')
+		next(faculty_csv)
+
+		for row in faculty_csv:
+			names = row[0]
+			degrees = row[1].strip()
+			titles = row[2]
+			emails = row[3]
+
+			last_name = re.findall(r'[A-Z][a-z]*\S$', names)
+			first_name = re.findall(r'^[A-Z][a-z]*\S*', names)
+
+			name = zip(first_name, last_name)
+
+			for item in name:
+				professor_dict[item] = [degrees, titles, emails]
+
+	return professor_dict
+
+for key, value in sorted(new_prof().items(), key = lambda k: k[0][1])[:3]:
+	print (key, value)
