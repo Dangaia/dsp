68a14522b0a4863873efacd2b5ec9d2ea2e13bfb
Dangaia Sims <dangaia.sims@gmail.com>
2017-04-06 13:46:24 -0400

    Completed

--- python/advanced_python_csv.py
+++ python/advanced_python_csv.py
@@ -1,1 +1,13 @@
-PLACE YOUR CODE HERE
+import csv
+
+emails = []
+f = open('faculty.csv')
+faculty_csv = csv.reader(f)
+
+next(faculty_csv)
+for row in faculty_csv:
+    emails.append(row[3])  
+with open("emails.csv", "w") as f:
+    write_faculty_csv = csv.writer(f)
+    for email in emails:
+        write_faculty_csv.writerow([email])
