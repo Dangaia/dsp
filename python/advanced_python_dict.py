568a79d9df1d3b117cb0d39ed2cd675e3bb27654
Dangaia Sims <dangaia.sims@gmail.com>
2017-04-06 14:41:27 -0400

    Completed

--- python/advanced_python_dict.py
+++ python/advanced_python_dict.py
@@ -1,33 +1,66 @@
+#Q6:
+
 import csv
-import string
 import re
 
-with open('faculty.csv') as csv_file:
-    next(csv_file, None)
-    faculty_csv = csv.reader(csv_file, delimiter=',')
-    counts = dict()
-    for row in faculty_csv:
-        row1 = row[0]
-        degree = row[1]
-        degree = degree.strip()
-        title = row[2]
-        line = title.strip()
-        line = str(line)
-        x = re.split("(Professor)+", line)
-        x = list(filter(None, x))
-        del x[-1]
-        y = ' '.join(x)
-        email = row[3]
-        words = row1.split()
-        last_name = words[-1]
-        desc = list()
-        desc.append(degree)
-        desc.append(y)
-        desc.append(email)
-        if last_name not in counts: 
-            counts[last_name] = list()
-            counts[last_name].append(desc)
-        else:
-            counts[last_name].append(desc)
-                    
-print(counts)
+filename = 'faculty.csv'
+
+def make_faculty_dict():
+	faculty_dict = {}
+	with open(filename, 'r') as f:
+		faculty_csv = csv.reader(f, delimiter=',')
+		next(faculty_csv)
+
+		for row in faculty_csv:
+			names = row[0]
+			degrees = row[1].strip()
+			titles = row[2]
+			emails = row[3]
+
+			last_name = re.findall(r'[A-Z][a-z]*\S$', names)
+
+			for item in last_name:
+				if item not in faculty_dict:
+					faculty_dict[item] = [degrees, titles, emails]
+				else:
+					faculty_dict[item] = [[degrees, titles, emails], faculty_dict[item]]
+
+	return faculty_dict
+
+print (make_faculty_dict())
+
+#Q7: 
+
+import csv
+import re
+
+filename = 'faculty.csv'
+    
+def make_professor_dict():
+	professor_dict = {}
+	with open(filename, 'r') as f:
+		faculty_csv = csv.reader(f, delimiter=',')
+		next(faculty_csv)
+
+		for row in faculty_csv:
+			names = row[0]
+			degrees = row[1].strip()
+			titles = row[2]
+			emails = row[3]
+
+			last_name = re.findall(r'[A-Z][a-z]*\S$', names)
+			first_name = re.findall(r'^[A-Z][a-z]*\S*', names)
+
+			name = zip(first_name, last_name)
+
+			for item in name:
+				professor_dict[item] = [degrees, titles, emails]
+
+	return professor_dict
+
+for k, v in sorted(make_professor_dict().items())[:3]:
+	print (k, v)
+    
+#Q8:
+for k, v in sorted(make_prof().items(), key = lambda k: k[0][1])[:3]:
+	print (k, v)
