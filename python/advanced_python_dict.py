f9a0661fe164093bd778a9dba8ffcea75332da67
Dangaia Sims <dangaia.sims@gmail.com>
2017-04-06 15:38:47 -0400

    Completed

--- python/advanced_python_dict.py
+++ python/advanced_python_dict.py
@@ -8,25 +8,20 @@ filename = 'faculty.csv'
 faculty_dict = {}
 
 def new_faculty_dict():
-	
 	with open(filename, 'r') as f:
 		faculty_csv = csv.reader(f, delimiter=',')
 		next(faculty_csv)
-
 		for row in faculty_csv:
 			names = row[0]
 			degrees = row[1].strip()
 			titles = row[2]
 			emails = row[3]
-
 			last_name = re.findall(r'[A-Z][a-z]*\S$', names)
-
 			for item in last_name:
 				if item not in faculty_dict:
 					faculty_dict[item] = [degrees, titles, emails]
 				else:
 					faculty_dict[item] = [[degrees, titles, emails], faculty_dict[item]]
-
 	return faculty_dict
 
 for key, value in sorted(new_faculty_dict().items())[:3]:
@@ -45,21 +40,16 @@ def new_professor_dict():
 	with open(filename, 'r') as f:
 		faculty_csv = csv.reader(f, delimiter=',')
 		next(faculty_csv)
-
 		for row in faculty_csv:
 			names = row[0]
 			degrees = row[1].strip()
 			titles = row[2]
 			emails = row[3]
-
 			last_name = re.findall(r'[A-Z][a-z]*\S$', names)
 			first_name = re.findall(r'^[A-Z][a-z]*\S*', names)
-
 			name = zip(first_name, last_name)
-
 			for item in name:
 				professor_dict[item] = [degrees, titles, emails]
-
 	return professor_dict
 
 for key, value in sorted(new_professor_dict().items())[:3]:
@@ -77,21 +67,16 @@ def new_professor_dict():
 	with open(filename, 'r') as f:
 		faculty_csv = csv.reader(f, delimiter=',')
 		next(faculty_csv)
-
 		for row in faculty_csv:
 			names = row[0]
 			degrees = row[1].strip()
 			titles = row[2]
 			emails = row[3]
-
 			last_name = re.findall(r'[A-Z][a-z]*\S$', names)
 			first_name = re.findall(r'^[A-Z][a-z]*\S*', names)
-
 			name = zip(first_name, last_name)
-
 			for item in name:
 				professor_dict[item] = [degrees, titles, emails]
-
 	return professor_dict
 
 for key, value in sorted(new_prof().items(), key = lambda k: k[0][1])[:3]:
