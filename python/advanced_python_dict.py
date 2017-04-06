07148a3e7e22d009065c22cd5cdd01897332e1c4
Dangaia Sims <dangaia.sims@gmail.com>
2017-04-06 13:52:39 -0400

    Update advanced_python_dict.py

--- python/advanced_python_dict.py
+++ python/advanced_python_dict.py
@@ -1,1 +1,33 @@
-PLACE YOUR CODE HERE
+import csv
+import string
+import re
+
+with open('faculty.csv') as csv_file:
+    next(csv_file, None)
+    faculty_csv = csv.reader(csv_file, delimiter=',')
+    counts = dict()
+    for row in faculty_csv:
+        row1 = row[0]
+        degree = row[1]
+        degree = degree.strip()
+        title = row[2]
+        line = title.strip()
+        line = str(line)
+        x = re.split("(Professor)+", line)
+        x = list(filter(None, x))
+        del x[-1]
+        y = ' '.join(x)
+        email = row[3]
+        words = row1.split()
+        last_name = words[-1]
+        desc = list()
+        desc.append(degree)
+        desc.append(y)
+        desc.append(email)
+        if last_name not in counts: 
+            counts[last_name] = list()
+            counts[last_name].append(desc)
+        else:
+            counts[last_name].append(desc)
+                    
+print(counts)
